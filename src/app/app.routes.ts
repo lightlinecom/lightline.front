6794db9e9b0ced5f88d7cb3cc72651203b134107
Gerald <geraldbustosm@gmail.com>
2026-03-27 16:10:55 -0300

    formal style to timeline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Layout } from './shared/layout/layout';
+import { Timeline } from './pages/timeline/timeline';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'timeline',
+    component: Layout,
+    children: [
+      {
+        path: ':id',
+        component: Timeline
+      }
+    ]
+  }
+];
---
+++ src/app/pages/timeline/timeline.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline',
+  imports: [],
+  templateUrl: './timeline.html',
+  styles: ``,
+})
+export class Timeline {}
