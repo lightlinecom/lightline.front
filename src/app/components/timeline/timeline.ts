8056a6584febb45002a582c2cd36224f1404afa6
Gerald <geraldbustosm@gmail.com>
2026-03-29 13:24:32 -0300

    add footer

--- src/app/components/timeline/timeline.ts
+++ src/app/components/timeline/timeline.ts
@@ -2,11 +2,10 @@ import { Component, input } from '@angular/core';
 import { TimelineItem } from "../timeline-item/timeline-item";
 import { TimelineEvent } from '../../interfaces/timeline-event';
 import { TimelineNavigation } from '../timeline-navigation/timeline-navigation';
-import { TimelineLiveIndicator } from '../timeline-live-indicator/timeline-live-indicator';
 
 @Component({
   selector: 'app-timeline',
-  imports: [TimelineLiveIndicator, TimelineItem, TimelineNavigation],
+  imports: [TimelineItem, TimelineNavigation],
   templateUrl: './timeline.html'
 })
 export class Timeline {
--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "../header/header";
 import { TimelineLiveIndicator } from '../../../components/timeline-live-indicator/timeline-live-indicator';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header, TimelineLiveIndicator],
+  imports: [RouterOutlet, Header, TimelineLiveIndicator, Footer],
   templateUrl: './layout.html'
 })
 export class Layout {}
